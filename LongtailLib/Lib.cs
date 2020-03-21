e3e41ea973ad17fbaccebc2f3c2227796006aea8
Dan Engelbrecht <dan.engelbrecht@embark-studios.com>
2020-03-21 11:02:53 +0100

    Simplified callback logic Added convenicence api ProgressHandle

--- LongtailLib/Lib.cs
+++ LongtailLib/Lib.cs
@@ -3,12 +3,16 @@ using System.Runtime.InteropServices;
 
 namespace Longtail
 {
-    public struct LongtailAPI { }
-    public struct VersionIndex { }
-    public struct ProgressAPI { }
+    public interface ProgressAPI
+    {
+        void OnProgress(uint total_count, uint done_count);
+    };
+    public struct Native_LongtailAPI { }
+    public struct Native_VersionIndex { }
+    public struct Native_ProgressAPI { }
 
     [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    public unsafe delegate void DisposeFunc(LongtailAPI* api);
+    public unsafe delegate void DisposeFunc(Native_LongtailAPI* api);
 
     [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate void AssertCallback(string expression, string file, int line);
@@ -17,16 +21,52 @@ namespace Longtail
     public unsafe delegate void LogCallback(void* context, int level, string str);
 
     [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    public unsafe delegate void ProgressCallback(ProgressAPI* progress_api, uint total_count, uint done_count);
+    public unsafe delegate void ProgressCallback(Native_ProgressAPI* progress_api, uint total_count, uint done_count);
 
     public class Lib
     {
+        public class ProgressHandle
+        {
+            public unsafe ProgressHandle(ProgressAPI progress)
+            {
+                m_Progress = progress;
+                ulong mem_size = Longtail_GetProgressAPISize();
+                byte* mem = (byte*)Longtail_Alloc(mem_size);
+                Longtail.DisposeFunc my_progress_dispose =
+                    (Longtail.Native_LongtailAPI* api) =>
+                    {
+                        Longtail.Lib.Longtail_Free(m_ProgressAPI);
+                    };
+                Longtail.ProgressCallback my_progress_callback =
+                    (Longtail.Native_ProgressAPI* progress_api, uint total_count, uint done_count) =>
+                    {
+                        m_Progress.OnProgress(total_count, done_count);
+                    };
+                m_ProgressAPI = Longtail.Lib.Longtail_MakeProgressAPI(mem, my_progress_dispose, my_progress_callback);
+            }
+            unsafe ~ProgressHandle()
+            {
+                Longtail_DisposeAPI((Native_LongtailAPI*)m_ProgressAPI);
+            }
+            unsafe public Native_ProgressAPI* AsProgressAPI()
+            {
+                return m_ProgressAPI;
+            }
+            ProgressAPI m_Progress;
+            unsafe Native_ProgressAPI* m_ProgressAPI;
+        };
+
+        public static unsafe void Progress_OnProgress(Longtail.Lib.ProgressHandle progress, uint total_count, uint done_count)
+        {
+            Longtail_Progress_OnProgress(progress.AsProgressAPI(), total_count, done_count);
+        }
+
 #if (DEBUG)
         [DllImport("longtail_debug.dll")]
 #else
         [DllImport("longtail.dll")]
 #endif
-        public unsafe static extern void Longtail_DisposeAPI(LongtailAPI* api);
+        public unsafe static extern void Longtail_DisposeAPI(Native_LongtailAPI* api);
 
 #if (DEBUG)
         [DllImport("longtail_debug.dll")]
@@ -69,7 +109,7 @@ namespace Longtail
 #else
         [DllImport("longtail.dll")]
 #endif
-        public unsafe static extern int Longtail_ReadVersionIndexFromBuffer(byte[] buffer, ulong size, ref VersionIndex* outVersionIndex);
+        public unsafe static extern int Longtail_ReadVersionIndexFromBuffer(byte[] buffer, ulong size, ref Native_VersionIndex* outVersionIndex);
 
 #if (DEBUG)
         [DllImport("longtail_debug.dll")]
@@ -83,13 +123,13 @@ namespace Longtail
 #else
         [DllImport("longtail.dll")]
 #endif
-        public unsafe static extern ProgressAPI* Longtail_MakeProgressAPI(void* mem, [MarshalAs(UnmanagedType.FunctionPtr)] DisposeFunc disposeFunc, [MarshalAs(UnmanagedType.FunctionPtr)] ProgressCallback progressCallback);
+        public unsafe static extern Native_ProgressAPI* Longtail_MakeProgressAPI(void* mem, [MarshalAs(UnmanagedType.FunctionPtr)] DisposeFunc disposeFunc, [MarshalAs(UnmanagedType.FunctionPtr)] ProgressCallback progressCallback);
 
 #if (DEBUG)
         [DllImport("longtail_debug.dll")]
 #else
         [DllImport("longtail.dll")]
 #endif
-        public unsafe static extern void Longtail_Progress_OnProgress(ProgressAPI* progressAPI, uint total_count, uint done_count);
+        public unsafe static extern void Longtail_Progress_OnProgress(Native_ProgressAPI* progressAPI, uint total_count, uint done_count);
     }
 }
--- LongtailLibTest/UnitTestCallbacks.cs
+++ LongtailLibTest/UnitTestCallbacks.cs
@@ -7,30 +7,22 @@ namespace LongtailLibTest
     [TestClass]
     public class UnitTestCallbacks
     {
-        public int LogLevel = -1;
-        public string LogString;
-
         [TestMethod]
         public unsafe void TestLogCallback()
         {
-            GCHandle objHandle = GCHandle.Alloc(this, GCHandleType.WeakTrackResurrection);
-            IntPtr context_ptr = GCHandle.ToIntPtr(objHandle);
-
+            int log_level = -1;
+            string log_string = "";
             Longtail.LogCallback myLogCallBack =
                 (void* context, int level, string str) =>
                 {
-                    UnitTestCallbacks obj = (UnitTestCallbacks)GCHandle.FromIntPtr((IntPtr)context).Target;
-                    if (obj != null)
-                    {
-                        obj.LogLevel = level;
-                        obj.LogString = str;
-                    }
+                    log_level = level;
+                    log_string = str;
                 };
             Longtail.Lib.Longtail_SetLogLevel(0);
-            Longtail.Lib.Longtail_SetLog(myLogCallBack, (void*)context_ptr);
+            Longtail.Lib.Longtail_SetLog(myLogCallBack, null);
             Longtail.Lib.Longtail_SetLogLevel(1);
-            Assert.AreEqual(LogString, "Longtail_SetLogLevel(1)");
-            Assert.AreEqual(LogLevel, 1);
+            Assert.AreEqual(log_level, 1);
+            Assert.AreEqual(log_string, "Longtail_SetLogLevel(1)");
         }
 
         [TestMethod]
@@ -38,7 +30,7 @@ namespace LongtailLibTest
         {
             Longtail.AssertCallback myAssertCallback = new Longtail.AssertCallback(AssertCB);
             Longtail.Lib.Longtail_SetAssert(myAssertCallback);
-            Longtail.VersionIndex* version_index = null;
+            Longtail.Native_VersionIndex* version_index = null;
             Longtail.Lib.Longtail_ReadVersionIndexFromBuffer(null, 0, ref version_index);
             Longtail.Lib.Longtail_SetAssert(null);
             Assert.AreEqual("buffer != 0", given_assert_expression);
@@ -57,23 +49,44 @@ namespace LongtailLibTest
             ulong progress_api_size = Longtail.Lib.Longtail_GetProgressAPISize();
             void* progress_api_mem = Longtail.Lib.Longtail_Alloc(progress_api_size);
             Longtail.DisposeFunc my_progress_dispose =
-                (Longtail.LongtailAPI* api) =>
+                (Longtail.Native_LongtailAPI* api) =>
                 {
                     Longtail.Lib.Longtail_Free(api);
                 };
             uint done = 0;
             uint total = 0;
             Longtail.ProgressCallback my_progress_callback =
-                (Longtail.ProgressAPI* progress_api, uint total_count, uint done_count) =>
+                (Longtail.Native_ProgressAPI* progress_api, uint total_count, uint done_count) =>
                 {
                     done = done_count;
                     total = total_count;
                 };
-            Longtail.ProgressAPI* my_progress_api = Longtail.Lib.Longtail_MakeProgressAPI(progress_api_mem, my_progress_dispose, my_progress_callback);
+            Longtail.Native_ProgressAPI* my_progress_api = Longtail.Lib.Longtail_MakeProgressAPI(progress_api_mem, my_progress_dispose, my_progress_callback);
             Longtail.Lib.Longtail_Progress_OnProgress(my_progress_api, 100, 50);
-            Longtail.Lib.Longtail_DisposeAPI((Longtail.LongtailAPI*)(my_progress_api));
+            Longtail.Lib.Longtail_DisposeAPI((Longtail.Native_LongtailAPI*)(my_progress_api));
             Assert.AreEqual((uint)50, done);
             Assert.AreEqual((uint)100, total);
+        }
+
+        class MyProgress : Longtail.ProgressAPI
+        {
+            public void OnProgress(uint total_count, uint done_count)
+            {
+                total = total_count;
+                done = done_count;
+            }
+            public uint total = 0;
+            public uint done = 0;
+        };
+
+        [TestMethod]
+        public void TestProgressHandle()
+        {
+            MyProgress progress = new MyProgress();
+            Longtail.Lib.ProgressHandle progress_handle = new Longtail.Lib.ProgressHandle(progress);
+            Longtail.Lib.Progress_OnProgress(progress_handle, 100, 50);
+            Assert.AreEqual((uint)50, progress.done);
+            Assert.AreEqual((uint)100, progress.total);
         }
     }
 
