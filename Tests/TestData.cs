e2e39032b94dd4359bcdefe59f3dab9161275181
Justin Webb <webbju@users.noreply.github.com>
2022-09-03 10:57:55 +0200

    Fix unit test data and generalise utilities. (#14)
    * Fix totalcmd test data.
    
    * Generalize test utilities.

---
+++ Tests/TestData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO.Abstractions;
+
+namespace Tests
+{
+    internal static class TestData
+    {
+        public static string GetProjectRootPath(IFileSystem fileSystem)
+        {
+            string cwd = fileSystem.Directory.GetCurrentDirectory();
+            string projectRootDir = nameof(Tests);
+            while (!projectRootDir.Equals(fileSystem.Directory.GetParent(cwd).Name, StringComparison.InvariantCultureIgnoreCase))
+            {
+                cwd = fileSystem.Directory.GetParent(cwd).FullName;
+            }
+            return fileSystem.Directory.GetParent(cwd).FullName;
+        }
+
+        public static string GetTestDataPath(IFileSystem fileSystem)
+        {
+            return fileSystem.Path.Combine(GetProjectRootPath(fileSystem), "TestData");
+        }
+    }
+}
--- Tests/TestReadStoredBlock.cs
+++ Tests/TestReadStoredBlock.cs
@@ -12,23 +12,14 @@ namespace Tests
         public void ReadStoredBlock()
         {
             var localFS = new FileSystem();
-
-            // Ugly and evil hack to get to test folder, but this entire class is a test-hack so...
-            string cwd = localFS.Directory.GetCurrentDirectory();
-            string projectRootDir = "Tests";
-            while (!projectRootDir.Equals(localFS.Directory.GetParent(cwd).Name, StringComparison.InvariantCultureIgnoreCase))
-            {
-                cwd = localFS.Directory.GetParent(cwd).FullName;
-            }
-            string projectRoot = localFS.Directory.GetParent(cwd).FullName;
-            string sampleBlock = localFS.Path.Combine(new string[] { projectRoot, "TestData", "store", "chunks", "0c75", "0x0c752d07f7d8a2b7.lrb" });
+            string testDataPath = TestData.GetTestDataPath(localFS);
+            string sampleBlock = localFS.Path.Combine(new string[] { testDataPath, "store", "chunks", "0c75", "0x0c752d07f7d8a2b7.lrb" });
 
             byte[] readBuffer = File.ReadAllBytes(sampleBlock);
-            var storedBlock = LongtailLib.API.ReadStoredBlockFromBuffer(readBuffer);
-            storedBlock.Dispose();
+            using var storedBlock = LongtailLib.API.ReadStoredBlockFromBuffer(readBuffer);
 
-//            byte[] writeBuffer = LongtailLib.API.WriteStoredBlockToBuffer(storedBlock);
-//            Assert.AreEqual(readBuffer.Length, writeBuffer.Length);
+            byte[] writeBuffer = LongtailLib.API.WriteStoredBlockToBuffer(storedBlock);
+            Assert.AreEqual(readBuffer.Length, writeBuffer.Length);
         }
     }
 }
